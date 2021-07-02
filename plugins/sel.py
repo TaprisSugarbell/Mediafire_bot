8541257b29ee1b4eedbd12205a789cc18988e660
Sugarbell <anonesayu@gmail.com>
2021-07-02 01:49:16 -0600

    Mediafire Bot v0.00 # Test sel

--- plugins/sel.py
+++ plugins/sel.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 from pyrogram import Client, filters
-from selenium.webdriver.firefox.options import Options
 
 
 async def sel(bot, update):
     text = " ".join(update.text.split())
-    op = Options()
-    op.headless = True
-    with webdriver.Firefox(options=op) as driver:
+    op = webdriver.ChromeOptions()
+    op.add_argument("headless")
+    with webdriver.Chrome(options=op) as driver:
         driver.get(text)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         title = driver.find_element_by_xpath("//span[@id='folder_name']").text
