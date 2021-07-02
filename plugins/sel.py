6fede6a6299cb474aff2f2f11b99ca7e4911fa44
Sugarbell <anonesayu@gmail.com>
2021-07-02 02:03:21 -0600

    Mediafire Bot v0.00 # Add arguments & Update requirements

--- plugins/sel.py
+++ plugins/sel.py
@@ -1,12 +1,16 @@
+import os
 from selenium import webdriver
 from pyrogram import Client, filters
 
 
 async def sel(bot, update):
     text = " ".join(update.text.split())
-    op = webdriver.ChromeOptions()
-    op.add_argument("headless")
-    with webdriver.Chrome(options=op) as driver:
+    ops = webdriver.ChromeOptions()
+    ops.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    ops.add_argument("headless")
+    ops.add_argument("--no-sandbox")
+    ops.add_argument("--disable-dev-shm-usage")
+    with webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=ops) as driver:
         driver.get(text)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         title = driver.find_element_by_xpath("//span[@id='folder_name']").text
